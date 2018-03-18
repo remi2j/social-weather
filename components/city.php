73efc6c7c90960eedafd6a53e28f5d0e43f0ed52
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-03-18 19:44:30 +0100

    :bento: add rain gif

---
+++ components/city.php
@@ -0,0 +1,64 @@
+<div class="city">
+      <p class="city-name">Paris, France</p>
+      <div class="location-card">
+        <div class="friends">
+          <p class="label">Friends</p>
+          <div class="list">
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+            <p class="name">Lorem, ipsum.</p>
+          </div>
+        </div>
+        <div class="forecast">
+          <div class="label">Today, march 18th</div>
+          <div class="hours">
+            <div class="hour-forecast">
+              <p class="time">10 am</p>
+              <div class="details">
+                <p class="main">light intensity shower rain</p>
+                <p class="temperature">18 degrees</p>
+                <p class="humidity">42% humidity</p>
+                <p class="wind">1.2 mph</p>
+              </div>
+            </div>
+            <div class="hour-forecast">
+              <p class="time">10 am</p>
+              <div class="details">
+                <p class="main">light intensity shower rain</p>
+                <p class="temperature">18 degrees</p>
+                <p class="humidity">42% humidity</p>
+                <p class="wind">1.2 mph</p>
+              </div>
+            </div>
+            <div class="hour-forecast">
+              <p class="time">10 am</p>
+              <div class="details">
+                <p class="main">light intensity shower rain</p>
+                <p class="temperature">18 degrees</p>
+                <p class="humidity">42% humidity</p>
+                <p class="wind">1.2 mph</p>
+              </div>
+            </div>
+            <div class="hour-forecast">
+              <p class="time">10 am</p>
+              <div class="details">
+                <p class="main">light intensity shower rain</p>
+                <p class="temperature">18 degrees</p>
+                <p class="humidity">42% humidity</p>
+                <p class="wind">1.2 mph</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
--- index.php
+++ index.php
@@ -15,70 +15,7 @@
     <a href="#" class="connect">Connect with Twitter</div>
   </div> -->
   <section class="cities">
-    <div class="city">
-      <p class="city-name">Paris, France</p>
-      <div class="location-card">
-        <div class="friends">
-          <p class="label">Friends</p>
-          <div class="list">
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-            <p class="name">Lorem, ipsum.</p>
-          </div>
-        </div>
-        <div class="forecast">
-          <div class="label">Today, march 18th</div>
-          <div class="hours">
-            <div class="hour-forecast">
-              <p class="time">10 am</p>
-              <div class="details">
-                <p class="main">light intensity shower rain</p>
-                <p class="temperature">18 degrees</p>
-                <p class="humidity">42% humidity</p>
-                <p class="wind">1.2 mph</p>
-              </div>
-            </div>
-            <div class="hour-forecast">
-              <p class="time">10 am</p>
-              <div class="details">
-                <p class="main">light intensity shower rain</p>
-                <p class="temperature">18 degrees</p>
-                <p class="humidity">42% humidity</p>
-                <p class="wind">1.2 mph</p>
-              </div>
-            </div>
-            <div class="hour-forecast">
-              <p class="time">10 am</p>
-              <div class="details">
-                <p class="main">light intensity shower rain</p>
-                <p class="temperature">18 degrees</p>
-                <p class="humidity">42% humidity</p>
-                <p class="wind">1.2 mph</p>
-              </div>
-            </div>
-            <div class="hour-forecast">
-              <p class="time">10 am</p>
-              <div class="details">
-                <p class="main">light intensity shower rain</p>
-                <p class="temperature">18 degrees</p>
-                <p class="humidity">42% humidity</p>
-                <p class="wind">1.2 mph</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php include "components/city.php" ?>
   </section>
 </body>
 </html>
