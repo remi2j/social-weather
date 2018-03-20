7c55bd3bcd8c94b3dc7606bf5b4fb39e7c07121b
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-03-20 16:13:26 +0100

    :zap: cache requests to twitter API

--- includes/user.php
+++ includes/user.php
@@ -3,19 +3,31 @@
 include 'api-keys.php';
 include 'twitter-wrapper.php';
 
-$friendsURL = 'https://api.twitter.com/1.1/friends/list.json';
-$requestMethod = 'GET';
-$userParam = '?screen_name=' .$_GET['handle'];
+// Check if cached data exists
+$cachePath = '../cache/' .$_GET['handle']. '.txt';
 
-$twitter = new TwitterAPIExchange($settings);
+if (file_exists($cachePath)) {
+  // Get friends from cache
+  echo 'from cache <br>';
+  $friends = file_get_contents($cachePath);
+  $friends = json_decode($friends);
+} else {
+  // Get friends from Twitter API
+  echo 'from API <br>';
+  // Prepare request parameters
+  $twitter = new TwitterAPIExchange($settings);
+  $friendsURL = 'https://api.twitter.com/1.1/friends/list.json';
+  $requestMethod = 'GET';
+  $userParam = '?screen_name=' .$_GET['handle'];
 
-echo $twitter
-  ->setGetfield($userParam)
-  ->buildOauth($friendsURL, $requestMethod)
-  ->performRequest();
+  // Make the request
+  $friends = $twitter
+    ->setGetfield($userParam)
+    ->buildOauth($friendsURL, $requestMethod)
+    ->performRequest();
 
+  // Cache for future sessions
+  file_put_contents($cachePath, json_encode($friends));
+}
 
-
-
-
-//echo 'Hello there, ' .$_GET['handle']. '!';
+echo $friends;
