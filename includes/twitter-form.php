7606a23b85f8d01c83edc9507b9603a9348bc366
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-03-25 22:16:21 +0200

    :bug: fixed relative links

--- includes/twitter-form.php
+++ includes/twitter-form.php
@@ -1,5 +1,15 @@
-<form class="twitter-form" action="<?= $_SERVER['HTTP_REFERER'] ?>includes/user.php" method="get">
-    <label for="handle" class="at">@</label>
-    <input type="search" name="handle" id="handle" class="search" placeholder="username" autofocus>
-    <input type="submit" class="go" value="Go">
-  </form>
+<?php
+  $path = getcwd();
+  // Adapt path to working directory
+  if (stripos($path, 'includes') !== false) {
+    $path = 'user.php';
+  } else {
+    $path = 'includes/user.php';
+  }
+?>
+
+<form class="twitter-form" action="<?= $path ?>" method="get">
+  <label for="handle" class="at">@</label>
+  <input type="search" name="handle" id="handle" class="search" placeholder="username" autofocus>
+  <input type="submit" class="go" value="Go">
+</form>
