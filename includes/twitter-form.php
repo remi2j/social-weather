61d09eebe4c89b54e214500948622c9d7f72ca57
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-03-25 21:53:54 +0200

    :bug: fixed relative path fails

--- includes/twitter-form.php
+++ includes/twitter-form.php
@@ -1,4 +1,4 @@
-<form class="twitter-form" action="includes/user.php" method="get">
+<form class="twitter-form" action="<?= $_SERVER['HTTP_REFERER'] ?>includes/user.php" method="get">
     <label for="handle" class="at">@</label>
     <input type="search" name="handle" id="handle" class="search" placeholder="username" autofocus>
     <input type="submit" class="go" value="Go">
